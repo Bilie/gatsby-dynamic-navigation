a80de55a666c4ecb3d175d67dc98dedfb704b1f2
Bilie <bilianavaleva@gmail.com>
2019-01-06 19:24:09 +0100

    Display menu items with gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,71 @@
-import { Link } from 'gatsby'
+import { StaticQuery, Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: {
+          sourceInstanceName: {eq: "pages"},
+          name: {regex: "/^(?!index|404).*$/"}
+        }) {
+          edges {
+            node {
+              id
+              name
+              relativeDirectory
+            }
+          }
+        }
+      }`
+    }
+    render={data => {
+      return (
+        <div
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            background: `rebeccapurple`,
+            marginBottom: `1.45rem`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `1.45rem 1.0875rem`,
+            }}
+          >
+            <h1 style={{ margin: 0 }}>
+              <Link
+                to="/"
+                style={{
+                  color: `white`,
+                  textDecoration: `none`,
+                }}
+              >
+                {siteTitle}
+              </Link>
+            </h1>
+
+            <ul>
+            {
+              data.allFile.edges.map((page, i) =>{
+                return (
+                  <li key={page.node.id}>
+                    <Link
+                      to={page.node.name}>
+                      {page.node.name}
+                    </Link>
+                  </li> 
+                )
+              })
+            }
+            </ul>
+          </div>
+        </div>
+      )}
+    }
+  />
 )
 
 Header.propTypes = {
