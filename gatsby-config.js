f2d1d79470a12f0a1cf4d32faef80c637a6020c2
Bilie <bilianavaleva@gmail.com>
2019-01-07 00:22:09 +0100

    Add gatsby-transformer-remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,15 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
-    `gatsby-transformer-javascript-frontmatter`
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `docs`,
+        path: `${__dirname}/src/docs/`,
+      },
+    },
+    `gatsby-transformer-javascript-frontmatter`,
+    `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
