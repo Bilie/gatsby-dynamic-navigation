c23b26be4eacaefbab7457bddfa5e181e6e52331
Bilie <bilianavaleva@gmail.com>
2019-01-07 01:00:41 +0100

    Add documentation landing page

---
+++ src/pages/docs.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Sidebar from '../components/sidebar'
+
+const DocsPage = () => (
+  <Layout>
+    <SEO title="Docs" />
+    <Sidebar />
+    <section>
+      <h1>Documentation</h1>
+    </section>
+  </Layout>
+)
+
+export const frontmatter = {
+  title: 'Page 2',
+  path: 'page-2'
+}
+
+export default DocsPage
