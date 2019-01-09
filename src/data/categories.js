2ae4ca1ce06fa1880056e349e003d9176fa25529
Bilie <bilianavaleva@gmail.com>
2019-01-09 01:58:27 +0100

    Add categories list

---
+++ src/data/categories.js
@@ -0,0 +1,20 @@
+const categories = {
+  'docs': {
+    name: 'Documentation',
+    id: 34567
+  },
+  'contributing': {
+    name: 'Contribute',
+    id: 43567
+  },
+  'guides': {
+    name: 'Guides',
+    id: 456789
+  },
+  'api': {
+    name: 'API',
+    id: 9456
+  }
+}
+
+export default categories;
