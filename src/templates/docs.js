5b30283825a4c4b605b1fde57ab414e3be765995
Bilie <bilianavaleva@gmail.com>
2019-01-09 01:58:18 +0100

    Add markdown template

---
+++ src/templates/docs.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Sidebar from '../components/sidebar'
+import SEO from '../components/seo'
+
+export default ({ data }) => {
+  const article = data.markdownRemark
+
+  return (
+    <Layout layoutType="sidebar">
+      <SEO title={article.frontmatter.title} />
+      <section className="content__main">
+        <Sidebar />
+        <section dangerouslySetInnerHTML={{ __html: article.html }} />
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
